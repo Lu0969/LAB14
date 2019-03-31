7829dbfa9815c6c3ab48207ca80401b5728810c4
Lu0969 <44342841+Lu0969@users.noreply.github.com>
2019-03-31 13:17:07 +0600

    Add files via upload

---
+++ Diagonal.java
@@ -0,0 +1,6 @@
+package com.company;
+
+public enum Diagonal {
+    MAIN_DIAGONAL,
+    SIDE_DIAGONAL
+}
---
+++ Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+
+public class Main {
+
+    public static void main(String args[]) {
+        Matrix squareMatrix = new Matrix(5, -20, 20);
+        squareMatrix.render();
+        int max1 = squareMatrix.getMaxItem(Diagonal.MAIN_DIAGONAL);
+        int max2 = squareMatrix.getMaxItem(Diagonal.SIDE_DIAGONAL);
+        int max = (max1 > max2) ? max1 : max2;
+        squareMatrix.setItem(max);
+        squareMatrix.render();
+    }
+}
